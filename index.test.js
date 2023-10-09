57ca643221f2d61c5ee6af7aa6a1ebfd18533105
silverwind <me@silverwind.io>
2023-10-09 20:43:39 +0200

    speed up vitest

--- index.test.js
+++ index.test.js
@@ -1,6 +1,8 @@
-import ports from "./index.json";
+import {readFileSync} from "node:fs";
 
 test("ports", () => {
+  // json import in vitest is horribly slow, this is about 50 times faster
+  const ports = JSON.parse(readFileSync(new URL("index.json", import.meta.url)));
   expect(ports["0/tcp"]).toMatchSnapshot();
   expect(ports["3306/tcp"]).toMatchSnapshot();
   expect(ports["53/udp"]).toMatchSnapshot();
